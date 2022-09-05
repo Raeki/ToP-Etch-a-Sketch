e2565913d26dfe73a4d740da585861fa285adede
Raeki <mkeithley@gmail.com>
2022-09-05 11:16:25 +0900

    remove console.log

--- onload.js
+++ onload.js
@@ -1,7 +1,5 @@
 (() => {
   window.addEventListener("load", () => {
-    console.log("page loaded");
-
     const sketch = new Sketch();
 
     const sketchContainer = document.getElementById("sketch-container");
